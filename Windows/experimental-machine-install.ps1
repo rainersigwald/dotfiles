4905e40a38b639d7327a4a1fc07d8ef4eebd47c2
Rainer Sigwald <rainer@sigwald.org>
2019-04-16 10:50:06 -0500

    Introduce Windows machine setup script

---
+++ Windows/experimental-machine-install.ps1
@@ -0,0 +1,41 @@
+# Install Scoop itself
+
+Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
+
+scoop bucket add extras
+
+# oreutils
+scoop install ripgrep
+scoop install fd
+
+scoop install fzf
+setx FZF_DEFAULT_COMMAND "fd --type file --hidden --no-ignore"
+
+scoop install bat
+New-Item -path $(Split-Path $(bat --config-file)) -ItemType Directory
+New-Item -Path $(bat --config-file) -Value "--map-syntax proj:xml`n--map-syntax targets:xml`n--map-syntax props:xml`n--no-ignore-vcs"
+
+# other utils
+scoop install less # bat wants it, useful in general
+
+# Azure CLI
+scoop install azure-cli
+
+az extension add --name azure-devops
+
+# az devops configure --defaults organization=https://dev.azure.com/devdiv
+# az devops configure --defaults project=DevDiv
+
+# Git
+
+git config --global user.name "Rainer Sigwald"
+git config --global user.email "raines@microsoft.com"
+
+# Note: diff-so-fancy locked to 1.2.0
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/so-fancy/diff-so-fancy/v1.2.0/diff-so-fancy -OutFile $env:USERPROFILE\scoop\shims\diff-so-fancy
+New-Item -ItemType Directory $env:USERPROFILE\scoop\shims\lib
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/so-fancy/diff-so-fancy/v1.2.0/lib/DiffHighlight.pm -OutFile $env:USERPROFILE\scoop\shims\lib\DiffHighlight.pm
+git config --global core.pager "'C:\\Program Files\\Git\\usr\\bin\\perl.exe`" -I '$env:USERPROFILE\\scoop\\shims\\lib' `"$env:USERPROFILE\\scoop\\shims\\diff-so-fancy`""
+
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.publish "!git push --set-upstream origin `$(git rev-parse --abbrev-ref HEAD)"
