5ffa8a6c6221917171d5312810955a6fa590c568
Rainer Sigwald <rainer@sigwald.org>
2019-05-10 11:57:18 -0500

    Switch to `date=human`

--- Windows/experimental-machine-install.ps1
+++ Windows/experimental-machine-install.ps1
@@ -37,5 +37,5 @@ New-Item -ItemType Directory $env:USERPROFILE\scoop\shims\lib
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/so-fancy/diff-so-fancy/v1.2.0/lib/DiffHighlight.pm -OutFile $env:USERPROFILE\scoop\shims\lib\DiffHighlight.pm
 git config --global core.pager "'C:\\Program Files\\Git\\usr\\bin\\perl.exe`" -I '$env:USERPROFILE\\scoop\\shims\\lib' `"$env:USERPROFILE\\scoop\\shims\\diff-so-fancy`""
 
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.lg "log --color --graph --date=human --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.publish "!git push --set-upstream origin `$(git rev-parse --abbrev-ref HEAD)"
