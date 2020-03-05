8ed723b4c79cac191ed4eeec1f749674f01ba66b
Rainer Sigwald <raines@microsoft.com>
2020-03-05 10:16:23 -0600

    posh-git for git completion

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,7 @@
 Invoke-Expression (&starship init powershell)
 
+Import-Module posh-git
+
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
     param($commandName, $wordToComplete, $cursorPosition)
         dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
