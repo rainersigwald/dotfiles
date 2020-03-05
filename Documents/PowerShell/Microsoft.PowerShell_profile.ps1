0dbc29d05068c0242c630c9fe4ce1cb485f9acfa
Rainer Sigwald <raines@microsoft.com>
2020-03-05 10:11:58 -0600

    arg completion for dotnet CLI

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,8 @@
 Invoke-Expression (&starship init powershell)
+
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+           [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
